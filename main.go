cc3644ba213d3da4719b64c3fd61c782ba4b35a3
Ivan_Murogin <muroginivan@gmail.com>
2025-09-14 17:13:46 +0300

    Entering the bot code into Github

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"BOT/telegram"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Quote []struct {
+	Q string `json:"q"`
+	A string `json:"a"`
+}
+
+func main() {
+	err := godotenv.Load("keys.env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot, err := telegram.NewHandler(os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	url := "https://zenquotes.io/api/random"
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode != 200 {
+		log.Fatal("trash")
+	}
+
+	var quote Quote
+	err = json.NewDecoder(resp.Body).Decode(&quote)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot.SendMessage(fmt.Sprintf("Quote: %s \nAuthor: %s", quote[0].Q, quote[0].A))
+
+	// select {}
+}
---
+++ telegram/telegram.go
@@ -0,0 +1,51 @@
+package telegram
+
+import (
+	"errors"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type Handler struct {
+	bot    *tgbotapi.BotAPI
+	chatID int64
+}
+
+func NewHandler(botToken string) (*Handler, error) {
+	bot, err := tgbotapi.NewBotAPI(botToken)
+	if err != nil {
+		return nil, err
+	}
+
+	chatID, err := listen(bot)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Handler{bot: bot, chatID: chatID}, nil
+}
+
+func listen(bot *tgbotapi.BotAPI) (int64, error) {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 30
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil {
+			chatID := update.Message.Chat.ID
+			return chatID, nil
+		}
+	}
+	return 0, errors.New("cant listen chatID")
+}
+
+func (h *Handler) SendMessage(message string) error {
+	msg := tgbotapi.NewMessage(h.chatID, message)
+
+	_, err := h.bot.Send(msg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
