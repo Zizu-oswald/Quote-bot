1c19c5ff5ed2d9827aa72725cf3011170db19cd4
Ivan_Murogin <muroginivan@gmail.com>
2025-09-22 17:43:11 +0300

    improve lang selection

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"encoding/json"
+	// "encoding/json"
 	"log"
-	"net/http"
+	// "net/http"
 	"os"
 
 	"github.com/Zizu-oswald/Quote-bot/telegram"
@@ -12,25 +12,8 @@ import (
 	"github.com/joho/godotenv"
 )
 
-type Quote []struct {
-	Q string `json:"q"`
-	A string `json:"a"`
-}
-
 func main() {
 	err := godotenv.Load("keys.env")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	url := "https://zenquotes.io/api/random"
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode != 200 {
-		log.Fatal("trash")
-	}
-
-	var quote Quote
-	err = json.NewDecoder(resp.Body).Decode(&quote)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -47,3 +30,20 @@ func main() {
 	}
 
 }
+
+// type Quote []struct {
+// 	Q string `json:"q"`
+// 	A string `json:"a"`
+// }
+
+// url := "https://zenquotes.io/api/random"
+// resp, err := http.Get(url)
+// if err != nil || resp.StatusCode != 200 {
+// 	log.Fatal("trash")
+// }
+
+// var quote Quote
+// err = json.NewDecoder(resp.Body).Decode(&quote)
+// if err != nil {
+// 	log.Fatal(err)
+// }
--- telegram/telegram.go
+++ telegram/telegram.go
@@ -8,10 +8,18 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+type ChatStruct struct {
+	ID   int64
+	Lang string
+}
+
+var Chat ChatStruct
+
 func HandleUpdate(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 	if update.Message != nil {
 		if update.Message.Text == "/start" {
-			msg := newMessageWithButtons(update.Message.Chat.ID, "Change language:", "English", "Русский")
+			Chat.ID = update.Message.Chat.ID
+			msg := newMessageWithButtons(Chat.ID, "Change language:", "English", "Русский")
 			bot.Send(msg)
 		}
 	}
@@ -34,9 +42,19 @@ func newMessageWithButtons(ID int64, messageText string, butt1text string, butt2
 }
 
 func handleCallback(b *tgbotapi.BotAPI, cq *tgbotapi.CallbackQuery) {
-	callback := tgbotapi.NewCallback(cq.ID, "Lang="+cq.Data)
+	// callback := tgbotapi.NewCallback(cq.ID, "Lang="+cq.Data)
+	callback := tgbotapi.NewCallback(cq.ID, "")
 	if _, err := b.Request(callback); err != nil {
 		log.Println("Error: ", err)
 	}
-	b.Send(tgbotapi.NewMessage(cq.Message.Chat.ID, "Lang: "+cq.Data))
+
+	switch cq.Data {
+	case "ru":
+		Chat.Lang = "ru"
+		b.Send(tgbotapi.NewMessage(Chat.ID, "Выбран язык: Русский"))
+	case "en":
+		Chat.Lang = "en"
+		b.Send(tgbotapi.NewMessage(Chat.ID, "Language selected: English"))
+	default:
+	}
 }
