9aa447a28d3c826a4a7ef91c5b85fb4d7504cc08
Ivan_Murogin <muroginivan@gmail.com>
2025-09-30 22:28:13 +0300

    correction translation to Russian

--- mymemory/mymemory.go
+++ mymemory/mymemory.go
@@ -14,18 +14,19 @@ type mymemResp struct {
 	} `json:"responseData"`
 }
 
-func TranslEngToRus(str string) (string, error){
-	str = "get?q=" + str + "&langpair=en|ru"
-	str = url.PathEscape(str) // для замены символов в запросе [" " -> %20]
+func TranslEngToRus(str string) (string, error) {
+	base := "https://api.mymemory.translated.net/get"
 
-	// urlReq := "https://api.mymemory.translated.net/get?q=%s&langpair=en|ru"
-	// urlReq := "https://api.mymemory.translated.net/get?q=%s&langpair=en%7Cru"
-	urlReq := "https://api.mymemory.translated.net/%s"
+	params := url.Values{}          // для параметров запроса
+	params.Set("q", str)            // параметры запроса
+	params.Set("langpair", "en|ru") // -^
 
+	fullURL := base + "?" + params.Encode()
 
-	resp, err := http.Get(fmt.Sprintf(urlReq, str))
+	resp, err := http.Get(fullURL)
+
 	if err != nil {
-	  return "", fmt.Errorf("error with mymemory translation [en -> ru]: %e", err)
+		return "", fmt.Errorf("error with mymemory translation [en -> ru]: %e", err)
 	}
 	if resp.StatusCode != 200 {
 		return "", fmt.Errorf("mymemory translation [en -> ru] with status code: %d", resp.StatusCode)
@@ -33,12 +34,12 @@ func TranslEngToRus(str string) (string, error){
 
 	jsonResp, err := io.ReadAll(resp.Body)
 	if err != nil {
-	  return "", fmt.Errorf("cant read body of response: %e", err)
+		return "", fmt.Errorf("cant read body of response: %e", err)
 	}
 	var structResp mymemResp
 	err = json.Unmarshal(jsonResp, &structResp)
 	if err != nil {
-	  return "", fmt.Errorf("cant unmarshall json: %e", err)
+		return "", fmt.Errorf("cant unmarshall json: %e", err)
 	}
 
 	return structResp.ResponseData.TranslatedText, nil
